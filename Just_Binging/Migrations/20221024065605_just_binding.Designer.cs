832bb65b307cd52b2e44121c01ac06366dd28519
NicolasR64 <nicolasravaux64@gmail.com>
2022-10-24 09:41:00 +0200

    update migration

--- Just_Binging/Migrations/20221024065605_just_binding.Designer.cs
+++ Just_Binging/Migrations/20221024065605_just_binding.Designer.cs
@@ -4,6 +4,7 @@ using Just_Binging.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Just_Binging.Migrations
 {
     [DbContext(typeof(Just_BingingContext))]
-    partial class Just_BingingContextModelSnapshot : ModelSnapshot
+    [Migration("20221024065605_just_binding")]
+    partial class just_binding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
