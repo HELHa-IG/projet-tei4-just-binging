b80004aac6213e17a12be00b78063d9bf5f91ede
NicolasR64 <nicolasravaux64@gmail.com>
2022-10-24 09:40:33 +0200

    add TokenWallet model

---
+++ Just_Binging/Models/TokenWallet.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Just_Binging.Models
+{
+    public class TokenWallet
+    {
+        [Key]
+        public int TokenWalletId { get; set; }
+        public string Token { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public virtual User user { get; set; }
+    }
+}
