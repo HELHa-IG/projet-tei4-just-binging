20ad68658c003df2396cc16b47c4fbe3dfc3b054
NicolasR64 <nicolasravaux64@gmail.com>
2022-10-24 09:40:06 +0200

    add service TokenService.cs

---
+++ Just_Binging/Services/TokenService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Just_Binging.Services
+{
+    public class TokenService
+    {
+        //public static bool isTokenValid(DbSet<Models.TokenWallet> TokenWallets)
+        //{
+            //string token = HttpContext.Current.Request.Headers.Get("x-auth-token");
+            //if (token.IsEmpty())
+            //{
+            //    return false,
+            //}
+        //}
+    }
+}
