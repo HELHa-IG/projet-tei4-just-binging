03fd06ca67767a2a3c092abb03efc0c25d213a81
Hugo Noël <la208083@student.helha.be>
2022-12-21 13:33:57 +0100

    Importation du module pour les routes + déclaration de la constante appRoutes

--- frontend/just-binging/src/app/app.module.ts
+++ frontend/just-binging/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = []
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
