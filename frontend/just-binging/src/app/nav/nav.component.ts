7383b12f94c7af3f9b9b7cbef622cefbe4af0dc9
Hugo Noël <la208083@student.helha.be>
2022-12-23 20:16:08 +0100

    ajout de la déconnexion

--- frontend/just-binging/src/app/nav/nav.component.ts
+++ frontend/just-binging/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.userService.logout();
   }
 
 }
